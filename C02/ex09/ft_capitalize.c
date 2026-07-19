69fa4a278b694174061c276306304cb1c7e8c30f
Yahya Khaldoune <ykhaldou@e1r9p15.1337.ma>
2026-07-19 15:58:34 +0100

    first_of_legedary_commits

---
+++ C02/ex09/ft_capitalize.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_capitalize.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykhaldou <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/07/19 10:06:13 by ykhaldou          #+#    #+#             */
+/*   Updated: 2026/07/19 15:32:07 by ykhaldou         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_is_alpha_num(char *str)
+{
+	if (*str >= 'A' && *str <= 'Z')
+		return (1);
+	else if (*str >= 'a' && *str <= 'z')
+		return (1);
+	else if (*str >= '0' && *str <= '9')
+		return (1);
+	return (0);
+}
+
+void	ft_lower(char *str)
+{
+	if (*str >= 'A' && *str <= 'Z')
+		*str += 32;
+}
+
+void	ft_upper(char *str)
+{
+	if (*str >= 'a' && *str <= 'z')
+		*str -= 32;
+}
+
+char	*ft_strcapitalize(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (ft_is_alpha_num(str[i]))
+	{
+		ft_upper(str[i]);
+	}
+	i++;
+	while (str[i])
+	{
+		if (ft_is_alpha_num(str[i]) && !(ft_is_alpha_num(str[i - 1])))
+		{
+			ft_upper(str[i]);
+		}
+		else
+			ft_lower(str[i]);
+		i++;
+	}
+	return (*str);
+}
