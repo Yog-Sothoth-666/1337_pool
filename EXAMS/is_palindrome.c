8abd4dd11464d3ebd8ea0667cc96c9afd90e42c2
Yahya Khaldoune <ykhaldou@e1r10p13.1337.ma>
2026-07-23 13:26:08 +0100

    1234321

--- EXAMS/is_palindrome.c
+++ EXAMS/is_palindrome.c
@@ -1,3 +1,33 @@
 #include <unistd.h>
 
+int	is_palindrome(int n)
+{
+	int tab[10];
+	int i = 0;
+	int j = 0;
+	if (n < 0)
+		return (0);
+	else
+	{
+		while (n)
+		{
+			tab[i++] = n % 10;
+			n /= 10;
+		}
+		i--;
+		while (j < i)
+		{
+			if (tab[i--] == tab[j++]);
+			else
+				return (0);
+		}
+	}
+	return (1);
+}
 
+int main()
+{
+	char is;
+	is = is_palindrome(234565437) + '0';
+	write(1, &is, 1);
+}
